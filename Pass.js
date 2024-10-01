eb7e4a1aee63559c52d0142dda62b5b46581df41
sakazukiAoi <52361879+sakazukiAoi@users.noreply.github.com>
2024-10-01 11:36:19 +0900

    Create Pass.js

---
+++ Pass.js
@@ -0,0 +1,9 @@
+<script>
+function checkPassword() {
+  var password = document.getElementById("password").value;
+  if (password == "kasa") {
+    location.href = "https://sakazukiaoi.github.io/TMC./home.html";
+  } else {
+  }
+}
+</script>
