6694103657c04f4d8d62df0c0b46a79b5842eabf
sakazukiAoi <52361879+sakazukiAoi@users.noreply.github.com>
2024-10-14 21:01:08 +0900

    Delete Pass.js

--- Pass.js
+++
@@ -1,9 +0,0 @@
-<script>
-function checkPassword() {
-  var password = document.getElementById("password").value;
-  if (password == "kasa") {
-    location.href = "https://sakazukiaoi.github.io/TMC./home.html";
-  } else {
-  }
-}
-</script>
